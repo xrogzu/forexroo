4012d26c264a5e2f14e3fa339241a60bb9b24b54
xuzewei <xuzewei_2012@126.com>
2017-05-29 18:45:49 +0800

    NavContainer.java

--- src/main/java/com/github/xuzw/forexroo/crm/ui/main/nav/NavContainer.java
+++ src/main/java/com/github/xuzw/forexroo/crm/ui/main/nav/NavContainer.java
@@ -22,11 +22,12 @@ public class NavContainer extends YList {
         add(new Nav("/icon/home.svg", "首页"));
         add(new NavGroup(new NavFolder("/icon/user.svg", "用户管理")).add(new SubNav("交易商")).add(new SubNav("经纪人")).add(new SubNav("代理商")));
         add(new Nav("/icon/order.svg", "订单管理"));
-        add(new Nav("/icon/trade.svg", "交易管理"));
-        add(new NavGroup(new NavFolder("/icon/audit.svg", "审核管理")).add(new SubNav("交易商开户申请审核")).add(new SubNav("交易商提现审核")).add(new SubNav("经纪商会员提现审核")).add(new SubNav("综合类会员提现审核")).add(new SubNav("经纪人会员申请审核")));
-        add(new Nav("/icon/risk.svg", "风险监控"));
-        add(new NavGroup(new NavFolder("/icon/money.svg", "资金管理")).add(new SubNav("经纪商会员")).add(new SubNav("综合类会员")).add(new SubNav("交易商会员")));
-        add(new Nav("/icon/commission.svg", "返佣管理"));
+        add(new NavGroup(new NavFolder("/icon/trade.svg", "交易管理")).add(new SubNav("交易大师")).add(new SubNav("复制交易")).add(new SubNav("机构交易")));
+        add(new NavGroup(new NavFolder("/icon/audit.svg", "审核管理")).add(new SubNav("交易商开户申请审核")).add(new SubNav("经纪人申请审核")).add(new SubNav("交易商出金审核")).add(new SubNav("代理商出金审核")));
+        add(new NavGroup(new NavFolder("/icon/risk.svg", "风险监控")).add(new SubNav("交易商监控")).add(new SubNav("持仓监控")));
+        add(new NavGroup(new NavFolder("/icon/money.svg", "资金管理")).add(new SubNav("交易商出入金")).add(new SubNav("代理商出金")));
+        add(new NavGroup(new NavFolder("/icon/commission.svg", "返佣管理")).add(new SubNav("经纪人记录")).add(new SubNav("返佣设置")));
+        add(new NavGroup(new NavFolder("/icon/control.svg", "控制管理")).add(new SubNav("管理员列表")).add(new SubNav("权限组管理")));
         add(new NavGroup(new NavFolder("/icon/message.svg", "消息管理")).add(new SubNav("提示设置")));
         add(new Nav("/icon/system.svg", "系统管理"));
         add(new Nav("/icon/log.svg", "日志管理"));
