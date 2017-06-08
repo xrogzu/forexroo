6d249d980edc90351f7c2b3ebed2fd9ee3743071
xuzewei <xuzewei_2012@126.com>
2017-06-08 20:19:03 +0800

    精简

--- src/main/webapp/script/tooltip.js
+++
@@ -1,7 +0,0 @@
-function tooltip(id, title) {
-    $('#' + id).tooltipster({
-        theme: 'tooltipster-light',
-        trigger: 'click',
-        side: ['right', 'bottom']
-    }).tooltipster('content', title).tooltipster('open');
-}
