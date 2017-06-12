2d7afbaf098ef8e586bda8c4472c11a0cf76a3f2
xuzewei <xuzewei_2012@126.com>
2017-06-12 13:12:49 +0800

    精简

--- src/main/java/com/github/xuzw/forexroo/app/service/Mt4MiddlewareService.java
+++ src/main/java/com/github/xuzw/forexroo/app/service/Mt4MiddlewareService.java
@@ -13,12 +13,4 @@ public class Mt4MiddlewareService {
         json.put("symbol", symbol);
         return ActiveMq.sendRequestAndAwait("Get_Tick_Last_Topic", json);
     }
-
-    public static JSONObject deposit(double amount, int mt4RealAccount, int type) throws InterruptedException {
-        JSONObject mt4Request = new JSONObject();
-        mt4Request.put("login", mt4RealAccount);
-        mt4Request.put("operationtype", type);
-        mt4Request.put("amount", amount);
-        return ActiveMq.sendRequestAndAwait("Deposit_User_Info_Topic", mt4Request);
-    }
 }
